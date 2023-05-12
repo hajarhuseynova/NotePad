b6c61f17916a9400fde61fc3eeaf1d3c5625d417
hajar298 <hajarih@code.edu.az>
2023-05-12 19:10:19 +0400

    end without default

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const textareaNote = document.getElementById("textareaNote");
 const saveButton = document.getElementById("saveButton");
 const cards = document.getElementById("cards");
 const rounded = document.querySelector(".rounded");
+const searchInput = document.getElementById("searchInput");
 
 const red = document.getElementById("red");
 const purple = document.getElementById("purple");
 const blue = document.getElementById("blue");
 const orange = document.getElementById("orange");
 const green = document.getElementById("green");
+
+const confirmRed = document.querySelector(".confirmRed");
+const confirmPurple = document.querySelector(".confirmPurple");
+const confirmBlue = document.querySelector(".confirmBlue");
+const confirmOrange = document.querySelector(".confirmOrange");
+const confirmGreen = document.querySelector(".confirmGreen");
 
 let selectedColor = "";
 function selectedBoxColor(color) {
@@ -33,7 +40,8 @@ saveButton.addEventListener("click", () => {
   p.textContent = textareaNote.value.trim();
 
   card.style.borderColor = selectedColor;
-  h4.style.backgroundColor = selectedColor;
+  let newcolor = selectedColor.replace(")", ",0.4)");
+  h4.style.backgroundColor = newcolor;
 
   if (
     inputNote.value.trim() != "" &&
@@ -42,23 +50,71 @@ saveButton.addEventListener("click", () => {
   ) {
     card.append(h4, p);
     cards.append(card);
+    inputNote.value = "";
+    textareaNote.value = "";
+    selectedColor = "";
+    display();
   }
-  inputNote.textContent = " ";
-  textareaNote.textContent = " ";
+  const remove = document.createElement("button");
+  remove.classList.add("remove");
+  remove.addEventListener("click", () => {
+    let result = confirm("Are you sure?");
+    if (result) {
+      card.remove();
+    }
+  });
+  remove.innerHTML = `
+  <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" class="Content_deleteBtn__mQsUR" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path></svg>
+  `;
+  h4.append(remove);
 });
 
 red.addEventListener("click", () => {
   selectedBoxColor("rgb(240, 98, 146)");
+  display();
+  confirmRed.style.display = "inherit";
 });
 purple.addEventListener("click", () => {
   selectedBoxColor("rgb(186, 104, 200)");
+  display();
+  confirmPurple.style.display = "inherit";
 });
 blue.addEventListener("click", () => {
   selectedBoxColor("rgb(79, 195, 247)");
+  display();
+  confirmBlue.style.display = "inherit";
 });
 orange.addEventListener("click", () => {
   selectedBoxColor("rgb(255, 213, 79)");
+  display();
+  confirmOrange.style.display = "inherit";
 });
 green.addEventListener("click", () => {
   selectedBoxColor("rgb(174, 213, 129)");
+  display();
+  confirmGreen.style.display = "inherit";
+});
+
+function display() {
+  confirmRed.style.display = "none";
+  confirmPurple.style.display = "none";
+  confirmBlue.style.display = "none";
+  confirmOrange.style.display = "none";
+  confirmGreen.style.display = "none";
+}
+
+searchInput.addEventListener("input", () => {
+  const searchValueTitle = searchInput.value.trim().toLowerCase();
+  const cardsArr = [...cards.children];
+  cardsArr.forEach((card) => {
+    const title = card
+      .querySelector("h4")
+      .textContent.trim()
+      .toLocaleLowerCase();
+    if (title.includes(searchValueTitle)) {
+      card.style.display = "inline-block";
+    } else {
+      card.style.display = "none";
+    }
+  });
 });
